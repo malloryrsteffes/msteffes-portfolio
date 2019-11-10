1edd9223641f28a3ff8da5e8f5315442c1b6884d
Mal <malrsteffes@gmail.com>
2019-11-10 11:19:34 -0500

    edits to header, footer

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,29 @@
 import React from 'react'
 
 import Footer from './Footer'
-import avatar from '../assets/images/avatar.jpg'
+import avatar from '../assets/images/headshotsmall.png'
 
+const listSyle = {
+    listStyleType: "none"
+}
 class Header extends React.Component {
     render() {
         return (
             <header id="header">
                 <div className="inner">
                     <a href="#" className="image avatar"><img src={avatar} alt="" /></a>
-                    <h1><strong>I am Strata</strong>, a super simple<br />
-                    responsive site template freebie<br />
-                    crafted by <a href="http://html5up.net">HTML5 UP</a>.</h1>
+                    <h1><strong>Hi! I'm Mallory.</strong></h1>
+                    <br></br>
+                    <h3>Skills</h3>
+                    <ul style={listSyle}>
+                        <li>HTML/CSS</li>
+                        <li>Javascript</li>
+                        <li>jQuery</li>
+                        <li>React</li>
+                        <li>Node.js</li>
+                        <li>SQL/noSQL</li>
+
+                    </ul>
                 </div>
                 <Footer />
             </header>
