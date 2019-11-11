6f0376cb02adf938c980564935a215b507314ae2
Mal <malrsteffes@gmail.com>
2019-11-10 19:13:21 -0500

    actual portfolio images and captions added. link next

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,10 @@ import Layout from '../components/layout'
 // import Lightbox from 'react-images'
 import Gallery from '../components/Gallery'
 
-import thumb01 from '../assets/images/thumbs/01.jpg'
-import thumb02 from '../assets/images/thumbs/02.jpg'
-import thumb03 from '../assets/images/thumbs/03.jpg'
-import thumb04 from '../assets/images/thumbs/04.jpg'
-import thumb05 from '../assets/images/thumbs/05.jpg'
-import thumb06 from '../assets/images/thumbs/06.jpg'
+import thumb01 from '../assets/images/thumbs/footprint.png'
+import thumb02 from '../assets/images/thumbs/nameIt.png'
+import thumb03 from '../assets/images/thumbs/gridironClicky.png'
+import thumb04 from '../assets/images/thumbs/crystaltheme.jpg'
 
 import full01 from '../assets/images/fulls/01.jpg'
 import full02 from '../assets/images/fulls/02.jpg'
@@ -24,43 +22,29 @@ const DEFAULT_IMAGES = [
     id: '1',
     source: full01,
     thumbnail: thumb01,
-    caption: 'Photo 1',
-    description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.',
+    caption: 'Footprint',
+    description: 'A full stack React app, built to encourage and increase environmental consciousness.',
   },
   {
     id: '2',
     source: full02,
     thumbnail: thumb02,
-    caption: 'Photo 2',
-    description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.',
+    caption: 'Name-It!',
+    description: 'A full-stack, light-hearted name generating app. Built with Javascript/jQuery, Node, mySQL.',
   },
   {
     id: '3',
     source: full03,
     thumbnail: thumb03,
-    caption: 'Photo 3',
-    description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.',
+    caption: 'The Gridiron Clicky Game',
+    description: 'A sporty take on the class memory game. Built with React.',
   },
   {
     id: '4',
     source: full04,
     thumbnail: thumb04,
-    caption: 'Photo 4',
-    description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.',
-  },
-  {
-    id: '5',
-    source: full05,
-    thumbnail: thumb05,
-    caption: 'Photo 5',
-    description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.',
-  },
-  {
-    id: '6',
-    source: full06,
-    thumbnail: thumb06,
-    caption: 'Photo 6',
-    description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.',
+    caption: 'Crystal Collector',
+    description: 'A simple game built primarily with Javascript and jQuery',
   },
 ]
 
